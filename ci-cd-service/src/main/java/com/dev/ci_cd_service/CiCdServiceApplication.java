81e88cb15491c2797f32425fb9efc605f490e274
ArpanAk <akarpan2005@gmail.com>
2026-03-19 13:42:52 +0530

    devflow structure push

---
+++ ci-cd-service/src/main/java/com/dev/ci_cd_service/CiCdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ci_cd_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdServiceApplication.class, args);
+	}
+
+}
---
+++ ci-cd-service/src/test/java/com/dev/ci_cd_service/CiCdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ci_cd_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
