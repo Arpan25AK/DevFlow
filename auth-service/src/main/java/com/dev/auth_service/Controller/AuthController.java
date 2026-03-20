e13d3e0272a3d092b44809ce1ca70a7897829373
ArpanAk <akarpan2005@gmail.com>
2026-03-20 13:43:50 +0530

    posted context paths

--- auth-service/src/main/java/com/dev/auth_service/Controller/AuthController.java
+++ auth-service/src/main/java/com/dev/auth_service/Controller/AuthController.java
@@ -3,6 +3,7 @@ package com.dev.auth_service.Controller;
 import com.dev.auth_service.Service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,12 @@ public class AuthController {
     public AuthController(AuthService authService){
         this.authService = authService;
     }
-
+    @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody AuthRequest request){
         String responsemessege = authService.registerUser(request.email(), request.password());
         return ResponseEntity.ok(responsemessege);
     }
-
+    @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody AuthRequest request){
         String token = authService.login(request.email(), request.password());
         return ResponseEntity.ok(token);
