c50b89ba40c12e3c639b21ed9abbc67209c28445
Arpan25AK <akarpan2005@gmail.com>
2026-03-26 17:57:08 +0530

    created kafkaListener Service

--- notification-service/src/main/java/com/dev/notification_service/service/KafkaListenerService.java
+++ notification-service/src/main/java/com/dev/notification_service/service/KafkaListenerService.java
@@ -1,4 +1,30 @@
 package com.dev.notification_service.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class KafkaListenerService {
+
+    private final EmailService emailService;
+    private final ObjectMapper objectMapper;
+    //inbuilt dto
+    public record projectEvent(String ownerEmail, String name){}
+
+    public void consumeProjectEventCreated(String message){
+        try{
+            log.info("kafka intercepted message: {}",message);
+
+            projectEvent event = objectMapper.readValue(message, projectEvent.class);
+
+            emailService.sendProjectCreateMail(event.ownerEmail, event.name);
+        }catch(Exception e){
+            log.error("error occurred during kafkaService" + e.getMessage());
+        }
+    }
 }
