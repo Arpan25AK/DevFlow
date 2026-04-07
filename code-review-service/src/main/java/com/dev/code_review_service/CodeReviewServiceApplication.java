904ab9ae9959fd1bf8f80c51bfa0e6f8565ef832
Arpan25AK <akarpan2005@gmail.com>
2026-04-07 19:26:34 +0530

    modified the application file and added some anotation for mutual connections between servers in main file

--- code-review-service/src/main/java/com/dev/code_review_service/CodeReviewServiceApplication.java
+++ code-review-service/src/main/java/com/dev/code_review_service/CodeReviewServiceApplication.java
@@ -2,8 +2,12 @@ package com.dev.code_review_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class CodeReviewServiceApplication {
 
 	public static void main(String[] args) {
