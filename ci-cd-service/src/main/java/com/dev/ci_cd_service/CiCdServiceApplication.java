bdc25ebc691c5fc75fa22e9758e7fc4fa6589c28
Arpan25AK <akarpan2005@gmail.com>
2026-04-11 19:25:48 +0530

    made minor changes

--- ci-cd-service/src/main/java/com/dev/ci_cd_service/CiCdServiceApplication.java
+++ ci-cd-service/src/main/java/com/dev/ci_cd_service/CiCdServiceApplication.java
@@ -1,10 +1,17 @@
 package com.dev.ci_cd_service;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CiCdServiceApplication {
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdServiceApplication.class, args);
