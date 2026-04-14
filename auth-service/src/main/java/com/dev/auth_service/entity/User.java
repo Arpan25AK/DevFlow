d7da2942fa56e8572765014522a9f079d626e3ac
Arpan25AK <akarpan2005@gmail.com>
2026-04-14 20:41:12 +0530

    made every endpoint in devflow run and got positive result

--- auth-service/src/main/java/com/dev/auth_service/entity/User.java
+++ auth-service/src/main/java/com/dev/auth_service/entity/User.java
@@ -7,17 +7,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
-@Table(name = "users")
+@Table(name = "devflow-users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Column(unique = true, nullable = false)
     private String email;
