6f0742a89ee5ce446698abd7fca3578160c49b3b
Arpan25AK <akarpan2005@gmail.com>
2026-04-15 17:38:06 +0530

    made minor changes to cicd pipeline and repo

--- ci-cd-service/src/main/java/com/dev/ci_cd_service/entity/PipelineRun.java
+++ ci-cd-service/src/main/java/com/dev/ci_cd_service/entity/PipelineRun.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import jakarta.persistence.Id;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @Entity
@@ -16,8 +17,8 @@ import java.time.LocalDateTime;
 public class PipelineRun {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     private String ownerEmail;
 
--- ci-cd-service/src/main/java/com/dev/ci_cd_service/repo/PipelineRunRepository.java
+++ ci-cd-service/src/main/java/com/dev/ci_cd_service/repo/PipelineRunRepository.java
@@ -4,7 +4,8 @@ import com.dev.ci_cd_service.entity.PipelineRun;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface PipelineRunRepository extends JpaRepository<PipelineRun, Long> {
+public interface PipelineRunRepository extends JpaRepository<PipelineRun, UUID> {
     List<PipelineRun> findByOwnerEmailAndNameOrderByTimeStampDesc(String ownerEmail, String name);
 }
