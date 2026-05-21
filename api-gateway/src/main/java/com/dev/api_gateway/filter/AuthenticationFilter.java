3c049020441ec0b90bb216a40790225c5d9c16c8
Arpan25AK <akarpan2005@gmail.com>
2026-05-21 22:41:44 +0530

    created a cors config file and added the cors method to allow in auth security

--- api-gateway/src/main/java/com/dev/api_gateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/dev/api_gateway/filter/AuthenticationFilter.java
@@ -23,6 +23,11 @@ public class AuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String path = request.getRequestURI();
 
         if (path.startsWith("/api/auth") || path.startsWith("/api/chat")) {
