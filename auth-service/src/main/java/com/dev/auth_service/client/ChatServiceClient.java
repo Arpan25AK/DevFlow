49ab9f755cb5a626cdcc6853f10d70a417a2e9d8
Arpan25AK <akarpan2005@gmail.com>
2026-07-24 18:41:28 +0530

    changed the method from patch to post for feign client to work

--- auth-service/src/main/java/com/dev/auth_service/client/ChatServiceClient.java
+++ auth-service/src/main/java/com/dev/auth_service/client/ChatServiceClient.java
@@ -1,12 +1,13 @@
 package com.dev.auth_service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "ChatServiceConfig")
 public interface ChatServiceClient {
 
-    @PatchMapping("/internal/chat/anonymize/{senderId}")
+    // POST, not PATCH - Feign's default client can't send PATCH at all.
+    @PostMapping("/internal/chat/anonymize/{senderId}")
     void anonymizeSender(@PathVariable("senderId") String senderId);
 }
