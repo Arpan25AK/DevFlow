3fb2a5cb6807c903c1ba4138c7dfee3aae997ad3
Arpan25AK <akarpan2005@gmail.com>
2026-07-24 18:34:57 +0530

    solved a minor bug in api authfilter code that broke signup and login path calls

--- api-gateway/src/main/java/com/dev/api_gateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/dev/api_gateway/filter/AuthenticationFilter.java
@@ -31,8 +31,8 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         String path = request.getRequestURI();
 
         boolean isPublicAuthEndpoint = path.equals("/api/auth/login") ||
-                path.equals("api/auth/signup") ||
-                path.equals("api/auth/refresh")||
+                path.equals("/api/auth/signup") ||
+                path.equals("/api/auth/refresh")||
                 path.equals("/api/auth/logout");
 
         if ( isPublicAuthEndpoint || path.startsWith("/api/chat")) {
